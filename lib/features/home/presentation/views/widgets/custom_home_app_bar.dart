1eeb4a6f4769a3a57a35240d2108ce5987cf4feb
hebbayasser <hebbayasser1234@gmail.com>
2025-02-09 09:29:25 +0200

    Refactor: Remove nested BlocBuilder of the same type

--- lib/features/home/presentation/views/widgets/custom_home_app_bar.dart
+++ lib/features/home/presentation/views/widgets/custom_home_app_bar.dart
@@ -30,47 +30,42 @@ class CustomHomeAppBar extends StatelessWidget {
           ),
           BlocBuilder<UserDataCubit, UserDataState>(
             builder: (context, state) {
-              return BlocBuilder<UserDataCubit, UserDataState>(
-                builder: (context, state) {
-                  if (state is UserDataSuccess) {
-                    return ListTile(
-                      horizontalTitleGap: 0,
-                      minVerticalPadding: 0,
-                      contentPadding:
-                          const EdgeInsets.symmetric(horizontal: 12),
-                      leading: CircleAvatar(
-                        radius: 32,
-                        backgroundImage: state.user.imgUrl != null &&
-                                state.user.imgUrl!.isNotEmpty
-                            ? CachedNetworkImageProvider(
-                                state.user.imgUrl!,
-                                errorListener: (e) {
-                                  const Icon(Icons.error);
-                                },
-                              )
-                            : const AssetImage('assets/images/no_image.jpg'),
-                      ),
-                      trailing: const CustomNotificationButton(),
-                      title: Text(
-                        'HELLO , ${state.user.name}',
-                        style: AppFonts.bodyText16.copyWith(
-                            color: Colors.white, fontWeight: FontWeight.bold),
-                      ),
-                      subtitle: Text(
-                        'Check Amazing Recipes...',
-                        style: AppFonts.caption.copyWith(
-                          color: Colors.white,
-                        ),
-                      ),
-                    );
-                  } else {
-                    return const Center(
-                        child: CircularProgressIndicator(
+              if (state is UserDataSuccess) {
+                return ListTile(
+                  horizontalTitleGap: 0,
+                  minVerticalPadding: 0,
+                  contentPadding: const EdgeInsets.symmetric(horizontal: 12),
+                  leading: CircleAvatar(
+                    radius: 32,
+                    backgroundImage: state.user.imgUrl != null &&
+                            state.user.imgUrl!.isNotEmpty
+                        ? CachedNetworkImageProvider(
+                            state.user.imgUrl!,
+                            errorListener: (e) {
+                              const Icon(Icons.error);
+                            },
+                          )
+                        : const AssetImage('assets/images/no_image.jpg'),
+                  ),
+                  trailing: const CustomNotificationButton(),
+                  title: Text(
+                    'HELLO , ${state.user.name}',
+                    style: AppFonts.bodyText16.copyWith(
+                        color: Colors.white, fontWeight: FontWeight.bold),
+                  ),
+                  subtitle: Text(
+                    'Check Amazing Recipes...',
+                    style: AppFonts.caption.copyWith(
                       color: Colors.white,
-                    ));
-                  }
-                },
-              );
+                    ),
+                  ),
+                );
+              } else {
+                return const Center(
+                    child: CircularProgressIndicator(
+                  color: Colors.white,
+                ));
+              }
             },
           ),
           const CustomSearchBarContainer(),
